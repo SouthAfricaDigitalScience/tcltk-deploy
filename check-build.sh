1582dff3443ae7dee5adfc0bfcff280e7c4cd2e3
Bruce Becker <brucellino@gmail.com>
2017-01-08 09:46:41 +0100

    Wrong install dir for check-build

--- check-build.sh
+++ check-build.sh
@@ -29,8 +29,8 @@ proc ModulesHelp { } {
 module-whatis "Sets the environment for using $NAME ($VERSION.) See https://github.com/SouthAfricaDigitalScience/tcltk-deploy"
 setenv TCL_VERSION $VERSION
 setenv TK_VERSION  $VERSION
-setenv TCL_DIR $::env(SOFT_DIR)
-setenv TK_DIR $::env(SOFT_DIR)
+setenv TCL_DIR /data/ci-build/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+setenv TK_DIR /data/ci-build/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
 prepend-path LD_LIBRARY_PATH $::env(TCL_DIR)/lib
 prepend-path PATH $::env(TCL_DIR)/bin
 MODULE_FILE
